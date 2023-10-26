efd1fab955a7ad06f57dc6870a01219c3737e32b
Anastasiia-Bondarchuk <125996065+AnastasiaBndr@users.noreply.github.com>
2023-10-26 15:24:53 +0300

    aaaa

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,64 @@
+class Key{
+    private signature:number;
+    constructor(){
+        this.signature=Math.random();
+    }
+
+    getSignature():number{
+        return this.signature;
+    }
+}
+
+class Person{
+    constructor(private key:Key){
+        this.key=key;
+    }
+
+    getKey():Key{
+        return this.key;
+    }
+
+}
+
+abstract class House{
+    protected door: boolean;
+    
+    protected tenants:Person[];
+
+    constructor(protected key:Key){
+        this.door=false;
+        this.key=key;
+        this.tenants=[];
+    }
+
+    comeIn(person:Person){
+        if(this.door){
+            this.tenants.push(person);
+            console.log("Welcome!")
+        }else console.log('Get out!')
+            
+    }
+
+    abstract openDoor(key:Key):void;
+}
+
+class MyHouse extends House{
+    protected door:boolean;
+    protected tenants: Array<Person>;
+    constructor(protected key:Key,){
+        super(key);
+
+}
+
+    openDoor(key: Key):void {
+        if(key.getSignature()===this.key.getSignature()){
+            this.door=true;
+            console.log('Door is open!')
+        }else console.log('Door is closed! Get out!')
+    }
+
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
